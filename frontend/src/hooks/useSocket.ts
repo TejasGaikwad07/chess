d0802cc12185be63cb33b9e4584bca72648f3e17
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-04-30 11:26:13 +0530

    fixed socket hook

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,27 +1,27 @@
 import { useEffect, useState } from 'react';
-import { useUser } from '@repo/store/useUser';
 
-const WS_URL = import.meta.env.VITE_APP_WS_URL ?? 'ws://localhost:8080';
+const WS_URL = 'ws://localhost:8080';
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const user = useUser();
+  
 
   useEffect(() => {
-    const ws = new WebSocket(`${WS_URL}?token=${user.token}`);
+    const ws = new WebSocket(WS_URL);
 
     ws.onopen = () => {
       setSocket(ws);
     };
 
     ws.onclose = () => {
+      console.log("disconnected")
       setSocket(null);
     };
 
     return () => {
       ws.close();
     };
-  }, [user]);
+  }, []);
 
   return socket;
 };
